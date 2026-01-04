1ed19f74dbae98490ce8778311513bbe5d8f94e2
Emagnu <ignisvictorious@gmail.com>
2026-01-04 00:03:21 +0000

    Add all tyoes of variables and print them out

---
+++ README.md
@@ -0,0 +1,8 @@
+## Freely adapted for educational purposes from:
+ 
+###  Bill Sky - The Computer Guy!
+ [Fortran 01: Introduction Fortran And Installing the Needed Tools (Windows and Linux)](https://www.youtube.com/watch?v=QAciXDISbMQ&list=PLdG1SnrYXH0FsJR99e_isGmXCvyfqLwpW&index=2)  
+[Fortran 02: Fortran Program structure and Defining Variables](https://www.youtube.com/watch?v=ZGj2gi8r5-s&list=PLdG1SnrYXH0FsJR99e_isGmXCvyfqLwpW&index=3)  
+[FORTRAN 03: Comments and Constants in FORTRAN](https://www.youtube.com/watch?v=v15X_he-sl8&list=PLdG1SnrYXH0FsJR99e_isGmXCvyfqLwpW&index=3)  
+
+
--- fortranvar.f90
+++ fortranvar.f90
@@ -8,6 +8,32 @@ program fortranVar
     !complex :: 
     integer:: iMyAge = 28
 
+    integer(kind=2) :: hwordInt
+    integer(kind=4) :: dwordInt
+    integer(kind=8) :: qwordInt
+
+    real(kind=4) :: aFloat
+    real(kind=8) :: aDouble
+    real(kind=16) :: aLongDouble
+
+    character(len=80) :: fullLine
+    character(len=32) :: fullName
+
+
+    fullLine = "This is a full line of data so let's see"
+    fullName = "Bill Sky Computer Guy"
+
+    print *, fullLine
+    print *, fullName
+
+    print *, huge(aFloat)
+    print *, huge(aDouble)
+    print *, huge(aLongDouble)
+
+    print *, huge(hwordInt)
+    print *, huge(dwordInt)
+    print *, huge(qwordInt)
+
     int1 = 0
     float1 = 123.45
     middleInitial = "J"
@@ -16,6 +42,7 @@ program fortranVar
     print *, float1
     print *, "My age is ", iMyAge
     print *, "Middle Initial: ", middleInitial
+    print *, "huge(float1): ", huge(float1)
     print *, "Integer value: ", int1
     print *, "Logical value: ", trueFalse   
 
